40fae35f8c36e2319bdd0282035e2e4d7b4769c2
Khubaib Shahid <139511144+Khubaib-Shahid@users.noreply.github.com>
2024-04-02 00:33:22 +0500

    cli countdown timer completed

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let time = await inquirer.prompt([
+    { message: "Enter minute", name: "minute", type: "number" },
+    { message: "Enter second", name: "second", type: "number" }
+]);
+let sec = setInterval(function () {
+    console.log(chalk.greenBright(time.minute + " : " + time.second));
+    time.second--;
+    if (time.second < 0) {
+        time.second = 59;
+        time.minute--;
+    }
+    if (time.minute < 0) {
+        clearInterval(sec);
+        console.log(chalk.greenBright("Time up"));
+    }
+}, 1000);
